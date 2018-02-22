7e0e6bd0bfd9c44524db30c9e8893888c476650c
kevin <kevintmoy@gmail.com>
2018-02-21 21:20:59 -0800

    add constants file

---
+++ JPM/Constants.m
@@ -0,0 +1,15 @@
+//
+//  Constants.m
+//  JPM
+//
+//  Created by Kevin on 2/21/18.
+//  Copyright © 2018 Kevin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define BASE_URL @"https://api.spacexdata.com/v2/launches/upcoming?"
+
+#define LAUNCH_YEAR_URL @"&launch_year="
+#define START_DATE_URL @"&start="
+#define END_DATE_URL @"&final="
