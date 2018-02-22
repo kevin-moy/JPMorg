789dbe44d583e9f224cf6ceac02ac3d3a07d18de
kevin <kevintmoy@gmail.com>
2018-02-21 21:21:07 -0800

    add custom cell

---
+++ JPM/LaunchCell.h
@@ -0,0 +1,21 @@
+//
+//  LaunchCell.h
+//  JPM
+//
+//  Created by Kevin on 2/21/18.
+//  Copyright © 2018 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Launch.h"
+
+@interface LaunchCell : UITableViewCell
+- (id)initWithLaunch:(Launch *)launchObject;
+@property (weak, nonatomic) IBOutlet UILabel *flightNumberLabel;
+@property (weak, nonatomic) IBOutlet UILabel *launchYearLabel;
+@property (weak, nonatomic) IBOutlet UILabel *launchSiteLabel;
+@property (weak, nonatomic) IBOutlet UILabel *launchDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rocketNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rocketTypeLabel;
+@property (nonatomic, strong) Launch *launch;
+@end
---
+++ JPM/LaunchCell.m
@@ -0,0 +1,30 @@
+//
+//  LaunchCell.m
+//  JPM
+//
+//  Created by Kevin on 2/21/18.
+//  Copyright © 2018 Kevin. All rights reserved.
+//
+
+#import "LaunchCell.h"
+
+@implementation LaunchCell
+
+- (id)initWithLaunch:(Launch *)launchObject {
+    self.launch = launchObject;
+    if (self = [super init]) {
+        [self setCellView:self.launch];
+    }
+    return self;
+}
+
+- (void)setCellView:(Launch *)launch {
+    self.flightNumberLabel.text = [NSString stringWithFormat:@"Flight: %.0f", launch.flightNumber];
+    self.launchYearLabel.text = [NSString stringWithFormat:@"Launch Year: %@", launch.year];
+    self.launchSiteLabel.text = [NSString stringWithFormat:@"Launch Site: %@", launch.site];
+    self.launchDateLabel.text = [NSString stringWithFormat:@"Launch Date: %@", launch.date];
+    self.rocketNameLabel.text = [NSString stringWithFormat:@"Rocket Name: %@", launch.rocket.name];
+    self.rocketTypeLabel.text = [NSString stringWithFormat:@"Rocket Type: %@", launch.rocket.type];
+}
+
+@end
